f4deebae0f63da2cb3f96e8771a571b767fca329
ajaykmr8684 <ajaykmr8684@gmail.com>
2023-01-10 23:41:30 +0530

    Nothing

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mongoose.connect(DB_URL)
     console.log("Database Connected.")
 })
 .catch((e) => {
-    console.log(`Oops, Mongo died. Errors: ${e}`)
+    console.log(`Oops, Mongo died. Issue: ${e}`)
 })
 
 app.use(express.json());
